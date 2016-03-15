ca31b8f99da7503b81bb0b43773ee427bffdbf65
Cernei Eugenius <eugen.cernei@gmail.com>
2016-03-16 00:15:48 +0200

    Some mid-night changes

---
+++ MIDPS_Lab2/Additional projects/TolkienWorld/Orc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Orc : IComparable
+{
+    public uint id { get; set; }
+    public float Power { get; set; }
+    public string Master { get; set; }
+
+    public Orc()
+    {
+
+    }
+    public override string ToString()
+    {
+        return String.Format("{0} nr. {1} with Master = {2}", "Orc", id, Master);
+    }
+    public int CompareTo(object obj)
+    {
+        return this.ToString().CompareTo(obj.ToString());
+    }
+}
