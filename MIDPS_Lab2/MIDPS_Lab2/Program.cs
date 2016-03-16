31dab2ee4c04e9e5ea1bb29a2dff3dcec7d2b953
Țibulica Oleg <nelsman37gmail.com>
2016-03-16 09:27:46 +0200

    from Univer

--- MIDPS_Lab2/MIDPS_Lab2/Program.cs
+++ MIDPS_Lab2/MIDPS_Lab2/Program.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using SQL;
 namespace MIDPS_Lab2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            SqlHandler sqlHandler = new SqlHandler();
+            sqlHandler.Connect();
         }
     }
 }
---
+++ MIDPS_Lab2/SqlHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace SQL
+{
+    public class SqlHandler
+    {
+        public SqlHandler()
+        {
+        }
+
+        public void Connect()
+        {
+            string connectionString = "user id=username;password=password;server=localhost;Trusted_Connection=yes;" +
+                                           "database=MiddleEarth; connection timeout=30";
+
+            SqlConnection myConnection = new SqlConnection(connectionString);
+            
+            try
+            {
+                myConnection.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.WriteLine("asda");
+            }
+
+        }
+    }
+
+
+
+
+}
