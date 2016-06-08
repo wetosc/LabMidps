a014a286986ec3fa8ce7c73901d85ffcb36b2ab0
Cernei Eugenius <eugen.cernei@gmail.com>
2016-06-08 23:16:59 +0300

    Modified a litlle lab 5, started lab 6.

---
+++ MIDPS_Lab5/Additional projects/DLLSpecial/DLLSpecial/User.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DLLSpecial
+{
+    public class User:SQLObject
+    {
+        public string name { get; set; }
+        public string password { get; set; }
+        public bool isAdmin { get; set; }
+        public bool canViewExtra { get; set; }
+        public bool canEdit { get; set; }
+        public User(int right)
+        {
+            switch (right)
+            {
+                case 1:
+                    isAdmin = false;
+                    canEdit = false;
+                    canViewExtra = false;
+                    break;
+                case 2:
+                    isAdmin = false;
+                    canEdit = false;
+                    canViewExtra = true;
+                    break;
+                case 3:
+                    isAdmin = false;
+                    canEdit = true;
+                    canViewExtra = true;
+                    break;
+                case 10:
+                    isAdmin = true;
+                    canEdit = true;
+                    canViewExtra = true;
+                    break;
+            }
+        }
+
+        public User()
+        {
+            isAdmin = false;
+            canEdit = false;
+            canViewExtra = false;
+        }
+
+
+
+        #region SQLObject
+        public string select()
+        {
+            return "use Users; SELECT * FROM Users";
+        }
+        public string select2(int id)
+        {
+            return "";
+        }
+        public string insertString()
+        {
+            string p1 = "UserName,Password, IsAdmin, CanViewExtra, CanEdit", p2 = String.Format("'{0}','{1}','{2}','{3}','{4}'", this.name, this.password, this.isAdmin?1:0, this.canViewExtra ? 1 : 0, this.canEdit ? 1 : 0 );
+            return String.Format("use Users; INSERT INTO Users ({0}) output INSERTED.ID VALUES ({1});", p1, p2);
+        }
+        public string deleteString(int n)
+        {
+            return String.Format("use Users; DELETE TOP({0}) FROM Users;", n);
+        }
+        public string deleteOneString(int id)
+        {
+            return String.Format("use Users; DELETE FROM Users WHERE id ='{0}';", id);
+        }
+        public string updateFormat()
+        {
+            return "use Users; UPDATE Users SET Password = '{0}', IsAdmin = '{1}', CanViewExtra = '{2}', CanEdit = '{3}'  WHERE id = '{4}'";
+        }
+        #endregion
+
+    }
+}
---
+++ MIDPS_Lab5/MIDPS_Lab4/MIDPS_Lab4/Login.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using DLLSpecial;
+namespace MIDPS_Lab5
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void login_Click(object sender, RoutedEventArgs e)
+        {
+            DataSet rghtx = Singleton.Instance.rightsForUser(username.Text, password.Password);
+            if (rghtx.Tables[0].Rows.Count >0)
+            {
+                MainWindow main = new MainWindow();
+                App.Current.MainWindow = main;
+                this.Close();
+                DataRow row = rghtx.Tables[0].Rows[0];
+                User temp = new User(); temp.isAdmin = (bool)row[3]; temp.canViewExtra = (bool) row[4]; temp.canEdit = (bool) row[5];
+                main.myController.model.currentUser = temp;
+                main.modifyButtons();
+                main.Show();
+            }
+        }
+    }
+}
